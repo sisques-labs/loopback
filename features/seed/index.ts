e7bc9190fea63a4bb2e4a6116399f9b9c5868241
JSisques <javierplazasisques@gmail.com>
2026-05-20 09:59:18 +0200

    refactor(seed): move lib helpers to own subdirs, remove index.ts files

--- features/seed/index.ts
+++
@@ -1,3 +0,0 @@
-export { PRESETS, isPresetSlug } from "./presets";
-export type { Preset, PresetSlug } from "./presets";
-export type { ServiceKey, ServiceResult, LoadReport, ResetReport } from "./types";
